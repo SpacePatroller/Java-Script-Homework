3b340d61d4dbc4d89450bb75587aecc9d8845a13
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-01-15 01:05:29 -0600

    Need to try bonus

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,45 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+// assign filter button to var
 var filterBtn = d3.select("#filter-btn");
 
-// Complete the click handler for the form
+// when the the filter button is pressed the following operations will be performed. 
 filterBtn.on("click", function () {
-
-  // Prevent the page from refreshing
-
-  d3.event.preventDefault();
+      
+    // if there is anydata when the button is clicked it will be removed. 
+    var tableRows = d3.selectAll("td").remove();
+        
+    // Prevent the page from refreshing ( TA Grading this -- whats the deal with this? Remember using it in the activities but forget the issues that could occur? It never really made a diffrence in my code.)
+    d3.event.preventDefault();
+    
+    // Select the input element and get the get the value property of the input element
+    var dateEntered = d3.select("#datetime").property("value");
+    console.log(dateEntered);
+ 
+    // Filter table by date entered. 
+    var dataFiltered = tableData.filter(date => date.datetime === String(dateEntered));   
+    console.log(dataFiltered);
   
-    // Select the input element and get the and Get the value property of the input element
-  var dateEntered = d3.select("#datetime").property("value");
-      console.log(dateEntered);
- 
-   // Filter table by date entered. 
     
-  var dataFiltered = tableData.filter(date => date.datetime === String(dateEntered));
-    
-  console.log(dataFiltered);
- 
+    // Reference to the table body
+    var tbody = d3.select("tbody");
+
+    dataFiltered.forEach((data) => {
+    // for each line of data add a new table row
+    var row = tbody.append("tr");
+        
+    // for each data item add add a td
+    Object.entries(data).forEach(([key, value]) => {
+        
+    // assign the method append to create a td cell
+    var cell = tbody.append("td");
+    // for each value create a td element within the row
+    cell.text(value);
+          });
+     }); 
 });
+
+
+
+
